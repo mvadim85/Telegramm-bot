2bef1763b5a1b42cb03297ba84e2c96f273196a8
Vadim Manyakin <mvadim85@gmail.com>
2024-05-13 23:49:51 +0300

    добавил тестовый файл

--- test.py
+++ test.py
@@ -8,17 +8,11 @@ from keyboards.for_questions import get_yes_no_kb
 
 # Запуск бота
 async def main(text):
-    bot = Bot(token=config.bot_token.get_secret_value(),  parse_mode="HTML")
-    dp = Dispatcher()
+    vot = Bot(token=config.bot_token.get_secret_value(),  parse_mode="HTML")
 
-    dp.include_routers(questions.router, different_types.router)
-    await bot.send_message(chat_id="615228120", text="sdfasfd", reply_markup=get_yes_no_kb())
+    await vot.send_message(chat_id="615228120", text="sdfasfd", reply_markup=get_yes_no_kb())
     # Запускаем бота и пропускаем все накопленные входящие
     # Да, этот метод можно вызвать даже если у вас поллинг
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
 
 
-if __name__ == "__main__":
-    text = "asdfasdf"
-    asyncio.run(main(text))
+asyncio.run(main(text))
